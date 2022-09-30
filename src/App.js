a078535ccfa29fc34aac7fcda973e33ce24f5ecb
sethu-ravichandran <rsethu1997@gmail.com>
2022-09-30 14:50:27 +0530

    Failing test case for intentional pipeline failure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          UnLearn React.!
+          Learn React.!
         </a>
       </header>
     </div>
